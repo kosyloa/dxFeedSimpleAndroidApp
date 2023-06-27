4b8e822f9049c72248a1861ee8b118d41565dcd7
AKosylo <alekskosylo@gmail.com>
2023-06-27 17:50:55 +0200

    remove default values

--- perftestapp/src/main/java/com/dxfeed/perftestapp/tools/Metrics.kt
+++ perftestapp/src/main/java/com/dxfeed/perftestapp/tools/Metrics.kt
@@ -2,8 +2,8 @@ package com.dxfeed.perftestapp.tools
 
 data class Metrics(
     val rateOfEvent: Double,
-    val rateOfListeners: Double = 0.0,
-    val numberOfEventsInCall: Double = 0.0,
+    val rateOfListeners: Double,
+    val numberOfEventsInCall: Double,
 
     val currentTime: Long
 )
