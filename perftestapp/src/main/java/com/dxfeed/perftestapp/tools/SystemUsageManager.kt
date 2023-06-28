5593b374f5ed31b20e9b63b7e2dc8eddd4238758
AKosylo <alekskosylo@gmail.com>
2023-06-28 15:57:53 +0200

    refactoring system manager

--- perftestapp/src/main/java/com/dxfeed/perftestapp/tools/SystemUsageManager.kt
+++ perftestapp/src/main/java/com/dxfeed/perftestapp/tools/SystemUsageManager.kt
@@ -7,12 +7,11 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 
-
 class SystemUsageManager(private val context: Context) {
     private val logger = Logging.getLogging(SystemUsageManager::class.java)
-    private val numberOfCores = Runtime.getRuntime().availableProcessors()
+    private val runtime = Runtime.getRuntime()
+    private val numberOfCores = runtime.availableProcessors()
     fun memoryUsage(): Long {
-        val runtime = Runtime.getRuntime();
         val usedMemInMB=(runtime.totalMemory() - runtime.freeMemory()) / 1048576L;
         return usedMemInMB
     }
@@ -22,17 +21,14 @@ class SystemUsageManager(private val context: Context) {
     }
 
     private fun sampleCPU(): Double {
-        println("start")
-        println("start 1")
         val lMyProcessID = Process.myPid()
-        val rate = 0
         try {
             var Result: String
             val p = Runtime.getRuntime().exec("top -o pid,%cpu -n 1 -p $lMyProcessID")
             val br = BufferedReader(InputStreamReader(p.getInputStream()))
             while (br.readLine().also { Result = it } != null) {
-                // replace "com.example.fs" by your application
                 if (Result.contains("$lMyProcessID")) {
+                    println(Result)
                     val info = Result.trim { it <= ' ' }
                         .replace(" +".toRegex(), " ").split(" ".toRegex())
                         .dropLastWhile { it.isEmpty() }
@@ -43,7 +39,6 @@ class SystemUsageManager(private val context: Context) {
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        println("finish")
-        return rate.toDouble()
+        return 0.0
     }
 }
