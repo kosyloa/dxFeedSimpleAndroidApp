fb73ef5a7bcc5f082f9834cc10d072dc5599ef73
AKosylo <alekskosylo@gmail.com>
2023-06-29 12:23:55 +0200

    support rotation(disable destroy/create on roation)

--- perftestapp/src/main/java/com/dxfeed/perftestapp/MainActivity.kt
+++ perftestapp/src/main/java/com/dxfeed/perftestapp/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.dxfeed.perftestapp
 
-import android.app.ActivityManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,20 +22,18 @@ import com.dxfeed.perftestapp.tools.SystemUsageManager
 
 
 class MainActivity : AppCompatActivity() {
-    private val symbols = listOf(
-    "ETH/USD:GDAX"
-    )
-    private val eventTypes = listOf(TimeAndSale::class.java)
 
-    private val adapter = QuoteAdapter(symbols)
-    private val speedometer: Speedometer by lazy {
-        Speedometer(2000, SystemUsageManager(this)) {
-            Handler(Looper.getMainLooper()).post {
-                adapter.reload(it)
-            }
+    private val speedometer = Speedometer(2000, SystemUsageManager()) {
+        Handler(Looper.getMainLooper()).post {
+            adapter.reload(it)
         }
     }
     private val service = QDService()
+    private val symbols = listOf("ETH/USD:GDAX")
+    private val eventTypes = listOf(TimeAndSale::class.java)
+
+    private val adapter = QuoteAdapter(symbols)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,24 +41,31 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        findViewById<TextView>(R.id.connectionTextView).text = DXEndpoint.State.NOT_CONNECTED.stringValue(this)
+        findViewById<TextView>(R.id.connectionTextView).text =
+            DXEndpoint.State.NOT_CONNECTED.stringValue(this)
 
         findViewById<Button>(R.id.connect_button)
             .setOnClickListener {
                 if (service.state == DXEndpoint.State.CONNECTED) {
                     service.disconnect()
                 } else {
-                    service.connect(findViewById<EditText>(R.id.address_text_view).text.toString(), symbols, eventTypes, connectionHandler = {
-                        Handler(Looper.getMainLooper()).post {
-                            val connectionTextView = findViewById<TextView>(R.id.connectionTextView);
-                            connectionTextView.text = it.stringValue(this)
-                            findViewById<Button>(R.id.connect_button).text  =
-                                if (it == DXEndpoint.State.CONNECTED) getString(R.string.disconnect)
-                                else getString(R.string.connect)
-                        }
-                    }, eventsHandler = {
-                        speedometer.update(it)
-                    })
+                    service.connect(
+                        findViewById<EditText>(R.id.address_text_view).text.toString(),
+                        symbols,
+                        eventTypes,
+                        connectionHandler = {
+                            Handler(Looper.getMainLooper()).post {
+                                val connectionTextView =
+                                    findViewById<TextView>(R.id.connectionTextView);
+                                connectionTextView.text = it.stringValue(this)
+                                findViewById<Button>(R.id.connect_button).text =
+                                    if (it == DXEndpoint.State.CONNECTED) getString(R.string.disconnect)
+                                    else getString(R.string.connect)
+                            }
+                        },
+                        eventsHandler = {
+                            speedometer.update(it)
+                        })
                     speedometer.cleanTime()
                 }
             }
--- perftestapp/src/main/java/com/dxfeed/perftestapp/tools/SystemUsageManager.kt
+++ perftestapp/src/main/java/com/dxfeed/perftestapp/tools/SystemUsageManager.kt
@@ -1,26 +1,25 @@
 package com.dxfeed.perftestapp.tools
 
-import android.content.Context
 import android.os.Process
 import com.devexperts.logging.Logging
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 
-class SystemUsageManager(private val context: Context) {
+class SystemUsageManager() {
     private val logger = Logging.getLogging(SystemUsageManager::class.java)
     private val runtime = Runtime.getRuntime()
     private val numberOfCores = runtime.availableProcessors()
     fun memoryUsage(): Long {
-        val usedMemInMB=(runtime.totalMemory() - runtime.freeMemory()) / 1048576L;
+        val usedMemInMB = (runtime.totalMemory() - runtime.freeMemory()) / 1048576L;
         return usedMemInMB
     }
 
     fun cpuUsage(): Double {
-        return sampleCPU() / numberOfCores
+        return getCpuStat() / numberOfCores
     }
 
-    private fun sampleCPU(): Double {
+    private fun getCpuStat(): Double {
         val lMyProcessID = Process.myPid()
         try {
             var result = ""
@@ -28,7 +27,6 @@ class SystemUsageManager(private val context: Context) {
             val br = BufferedReader(InputStreamReader(p.getInputStream()))
             while (br.readLine().also { result = it } != null) {
                 if (result.contains("$lMyProcessID")) {
-                    println(result)
                     val info = result.trim { it <= ' ' }
                         .replace(" +".toRegex(), " ").split(" ".toRegex())
                         .dropLastWhile { it.isEmpty() }
@@ -37,7 +35,7 @@ class SystemUsageManager(private val context: Context) {
                 }
             }
         } catch (e: IOException) {
-            e.printStackTrace()
+            logger.error("Get CPU stats error: ${e.message}")
         }
         return 0.0
     }
